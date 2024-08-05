f56d2833eb985b71e7fd9437664eb74a29830f25
Tshedza Nembilwi <tshedzaa@gmail.com>
2024-08-05 20:50:56 +0200

    This is a tip calculator

--- Tip_calculator.py
+++ Tip_calculator.py
@@ -11,7 +11,7 @@ total_bill = float(input("what is the total bill?: R"))
 tip_1 = int(input("How much tip (%) would you like to give? 10, 12, or 15? "))
 
 # ask the user how many people to split the bill
-num_people = int(input("How many people to split the bill?"))
+num_people = int(input("How many people/persons to split the bill?"))
 
 # Calculate the total tip amount
 tip_total = total_bill * (tip_1 / 100)
